9deb6eb7d77d4c7e57ab7efe60b693843e8efd31
Zunni Rojas <al21760406@ite.edu.mx>
2025-02-11 18:56:28 -0800

    Creacion de un cubo

---
+++ cubo.py
@@ -0,0 +1,39 @@
+"""
+Rojas Baldivia Zunni.
+Practica 1: Cubo.
+
+"""
+
+from turtle import *
+
+def cuadrado ():
+    forward(100),
+    right(90),
+    forward(100),
+    right(90),
+    forward(100),
+    right(90),
+    forward(100)
+
+cuadrado()
+
+right(45)
+forward(50)
+right(45)
+
+cuadrado()
+
+right(90)
+forward(100)
+right(135)
+forward(50)
+left(45)
+forward(100)
+left(135)
+forward(50)
+left(135)
+forward(100)
+left(45)
+forward(50)
+
+input("Detener tortuga")
