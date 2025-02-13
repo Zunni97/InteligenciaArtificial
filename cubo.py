8764413488b6c80905c4feefcb344aa5a7a30e9e
Zunni97 <al21760406@ite.edu.mx>
2025-02-12 21:26:01 -0800

    Se agrego una funcion para dibujar una linea

--- cubo.py
+++ cubo.py
@@ -3,8 +3,17 @@ Rojas Baldivia Zunni.
 Practica 1: Cubo.
 
 """
-
+import turtle as turtle
 from turtle import *
+
+def linea (num_pixeles):
+    for pixel in range(num_pixeles):
+        dot(3, "black")
+        turtle.forward(1)
+linea(100)
+
+turtle.teleport(x=-250)
+turtle.teleport(y=70)
 
 def cuadrado ():
     forward(100),
@@ -34,6 +43,6 @@ forward(50)
 left(135)
 forward(100)
 left(45)
-forward(50)
+forward(50) 
 
 input("Detener tortuga")
